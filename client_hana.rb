c2cfebdb3ba164b168777ac03d3bd8089d15aaca
Thomas Metz <thomasavuando@gmail.com>
2017-03-30 15:37:25 -0300

    Update client_hana.rb

--- client_hana.rb
+++ client_hana.rb
@@ -1,49 +1,42 @@
 require 'net/https'
-require 'json'
-require 'openssl'
-require 'date'
 
-# set to disable certification
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-uri = URI('https://IP:PORT/FILE_NAME.xsodata/FILE_NAME')
-req = Net::HTTP::Post.new uri.path
+module Hana
+  class XSodata
+    OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+    attr_accessor :body
 
-id_time = Time.now.to_i
-data_now = DateTime.now.strftime('%Q')
+    def initialize(path)
+      # set to disable certification
+      @uri = URI(path)
+    end
 
-req.body='{
-            "id":'+id_time.to_s+',
-            "agreement_id":"123",
-            "order_id":"564575686789",
-            "address_source":"ADDRESS",
-            "address_target":"ADDRESS",
-            "lat_address_source":"-29.6843729",
-            "lon_address_source":"-51.0438139",
-            "lat_address_target":"-23.6369921",
-            "lon_address_target":"-46.7550026",
-            "invoice_price":"1231231",
-            "price":"223332.0",
-            "company_id":"2",
-            "shipping_company_id":"2",
-            "saving":"0.0",
-            "status":"closed",
-            "date_source":"/Date('+data_now+')/",
-            "date_target":"/Date('+data_now+')/",
-            "vehicle_plate":"09876",
-            "driver_name":"JOHN"
-            }'
+    def get(param={})
+      req = Net::HTTP::Get.new @uri.path
+      call_service(req)
+    end
 
-puts req.body
+    def post(json_body)
+      req = Net::HTTP::Post.new @uri.path
+      req.body = json_body
+      call_service(req)
+    end
 
-req['Content-Type'] = 'application/json;odata=verbose;'
-req['Accept'] = 'application/json'
-req['content-type'] = 'application/json;charset=utf-8'
+    private
 
-res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-  #http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  http.ssl_version = :SSLv3
-  http.use_ssl = true
-  http.request req
+    def call_service(req)
+      set_header(req)
+
+      res = Net::HTTP.start(@uri.host, @uri.port, :use_ssl => @uri.scheme == 'https') do |http|
+        http.ssl_version = :SSLv3
+        http.use_ssl = true
+        http.request req
+      end
+      return res.body
+    end
+
+    def set_header(req)
+      req['Content-Type'] = 'application/json'
+      req['Accept'] = 'application/json'
+    end
+  end
 end
-
-puts res.body
